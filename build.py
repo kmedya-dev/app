f5b36d7f0a4c7659f41526cb70a3136f5912718a
kmedyfa-dev <koustavamedya.2000@gmail.com>
2025-06-30 17:19:57 +0530

    Initial commit

---
+++ build.py
@@ -0,0 +1,9 @@
+# App configuration
+APP_NAME = "ErudaBrowser"
+PACKAGE_NAME = "com.eruda.browser"
+VERSION_CODE = 1
+VERSION_NAME = "1.0"
+# Path to your icon within the project
+ICON_PATH = "icon.png"
+# The directory containing your web assets (index.html, etc.)
+WEB_ASSETS_PATH = "www"
